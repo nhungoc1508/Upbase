0edb22e1f022686dbea24104fa03fdebf22431b4
Ngoc N. Hoang <nnh245@nyu.edu>
2022-01-19 11:29:42 +0400

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/frest';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/upbase';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
